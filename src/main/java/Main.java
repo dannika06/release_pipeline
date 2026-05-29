7103ee3b8655b264f46a3507e678c082433bf461
Dannika <S-dannika.covarrubia@lwtech.edu>
2026-05-28 19:56:53 -0700

    Change greeting text in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String text = "Hello world";
+        String text = "Hello to the world";
         text = truncateString(text, 5);
         System.out.println(text);
     }
